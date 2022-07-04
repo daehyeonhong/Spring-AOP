a42b65783c9bea3555368d67ad403d4aeb6d2287
DaeHyeonHong <fholng@outlook.com>
2022-07-04 21:28:10 +0900

    구현 시작

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -1,9 +1,12 @@
 package hello.aop;
 
+import hello.aop.order.aop.AspectV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(value = {AspectV1.class})
 public class AopApplication {
 
     public static void main(String[] args) {
