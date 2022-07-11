ce0eb798a7870e099dd30ecc880c59d7cbc5f46c
DaeHyeonHong <fholng@outlook.com>
2022-07-11 21:43:27 +0900

    @Annotation

---
+++ src/test/java/hello/aop/pointcut/AtAnnotationTests.java
@@ -0,0 +1,38 @@
+package hello.aop.pointcut;
+
+import hello.aop.order.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(value = {AtAnnotationTests.AtAnnotationAspect.class}
+)
+class AtAnnotationTests {
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    @DisplayName(value = "success")
+    void success() {
+        log.info("memberService Proxy={}", this.memberService.getClass());
+        this.memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+        @Around("@annotation(hello.aop.order.aop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+            log.info("[@Annotation] {}", proceedingJoinPoint.getSignature());
+            return proceedingJoinPoint.proceed();
+        }
+    }
+}
