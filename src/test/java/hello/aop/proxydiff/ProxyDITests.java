1b4443b80b98f48eaa6f1b7d611ba5c1613b3fa8
DaeHyeonHong <fholng@outlook.com>
2022-07-17 14:59:29 +0900

    JDK Dynamic Proxy 한계2

--- src/test/java/hello/aop/proxydiff/ProxyDITests.java
+++ src/test/java/hello/aop/proxydiff/ProxyDITests.java
@@ -1,6 +1,7 @@
 package hello.aop.proxydiff;
 
 import hello.aop.order.member.MemberService;
+import hello.aop.order.member.MemberServiceImpl;
 import hello.aop.proxydiff.proxy.ProxyDIAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -10,19 +11,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK Dynamic Proxy
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK Dynamic Proxy
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB Proxy
 @Import(value = {ProxyDIAspect.class})
 class ProxyDITests {
     @Autowired
     MemberService memberService;
-//    @Autowired
-//    private MemberServiceImpl memberServiceImpl;
+    @Autowired
+    private MemberServiceImpl memberServiceImpl;
 
     @Test
     @DisplayName(value = "go")
-    void go() throws Exception {
+    void go() {
         log.info("memberService class {}", memberService.getClass());
-//        log.info("memberServiceImpl class {}", memberServiceImpl.getClass());
-//        memberServiceImpl.hello("hello");
+        log.info("memberServiceImpl class {}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
     }
 }
